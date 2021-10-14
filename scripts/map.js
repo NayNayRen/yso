1a4c0cbe73a8c76f7fcc2abded745fd69fbfdcb0
Nate M <jeditendencies35@gmail.com>
2021-10-14 17:35:22 -0400

    playing with custom map markers and info windows

--- scripts/map.js
+++ scripts/map.js
@@ -1,40 +1,51 @@
+
+// pinellas county lat and lng
+const myLocation = {
+  lat: 27.889647,
+  lng: -82.727766
+};
+const pins = [{
+    lat: 27.960969999438248,
+    lon: -82.76100309725183,
+    // address: 'Clearwater, FL',
+    // title: 'Tropical Smoothie Cafe',
+    info: `<div id='map-content'>
+    <span class='map-content-heading'>Tropical Smoothie</span>
+      <div class='map-content-address'>
+        <span class='map-content-street'>1840 Gulf to Bay Blvd.</span>
+        <span class='map-content-city'>Clearwater, FL 33765</span>
+      </div>
+    </div>`,
+  },
+  {
+    lat: 27.892476851843572,
+    lon: -82.78553762444348,
+    // address: 'Largo, FL',
+    // title: 'Largo Mall',
+    info: `<div id='map-content'>
+    <span class='map-content-heading'>Largo Mall</span>
+      <div class='map-content-address'>
+        <span class='map-content-street'>10500 Ulmerton Rd</span>
+        <span class='map-content-city'>Largo, FL 33771</span>
+      </div>
+    </div>`,
+  },
+  {
+    lat: 28.010112504139414,
+    lon: -82.72997017212303,
+    // address: 'Countryside, FL',
+    // title: "Duff's Buffet",
+    info: `<div id='map-content'>
+    <span class='map-content-heading'>Duff's Buffet</span>
+      <div class='map-content-address'>
+        <span class='map-content-street'>26111 US Hwy 19 N</span>
+        <span class='map-content-city'>Clearwater, FL 33763</span>
+      </div>
+    </div>`,
+  }
+];
+
 function loadMap() {
-  // pinellas county lat and lng
-  const myLocation = {
-    lat: 27.889647,
-    lng: -82.727766
-  };
-  const pins = [{
-      lat: 27.960969999438248,
-      lon: -82.76100309725183,
-      // address: 'Clearwater, FL',
-      // title: 'Tropical Smoothie Cafe',
-      info: `<div id='map-content'>
-      <span class='map-content-heading'>Tropical Smoothie</span>
-      <span class='map-content-address'>Clearwater, FL</span>
-      </div>`,
-    },
-    {
-      lat: 27.892476851843572,
-      lon: -82.78553762444348,
-      // address: 'Largo, FL',
-      // title: 'Largo Mall',
-      info: `<div id='map-content'>
-      <span class='map-content-heading'>Largo Mall</span>
-      <span class='map-content-address'>Largo, FL</span>
-      </div>`,
-    },
-    {
-      lat: 28.010112504139414,
-      lon: -82.72997017212303,
-      // address: 'Countryside, FL',
-      // title: "Duff's Buffet",
-      info: `<div id='map-content'>
-      <span class='map-content-heading'>Duff's Buffet</span>
-      <span class='map-content-address'>Countryside, FL</span>
-      </div>`,
-    }
-  ];
   // map centers on pinellas county
   const map = new google.maps.Map(document.getElementById("map"), {
     // zoomControl: false,
@@ -53,8 +64,8 @@ function loadMap() {
     });
     // for marker title and location
     const marker = new google.maps.Marker({
-      position: new google.maps.LatLng(pins[i].lat, pins[i].lon), map,
-      animation: google.maps.Animation.DROP,
+      position: new google.maps.LatLng(pins[i].lat, pins[i].lon),
+      map,
     });
     marker.addListener("click", () => {
       infoWindow.open({
@@ -63,9 +74,6 @@ function loadMap() {
         shouldFocus: false,
       });
     });
-    if(document.documentElement.scrollTop > 800){
-
-  }
   }
 }
 
