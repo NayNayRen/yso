1756f941e61d29565bfdcc7c9c48f9a7141f80a8
Nate M <jeditendencies35@gmail.com>
2021-11-12 15:35:33 -0500

    added and modded code for the bottom buttons and containers for the top

--- scripts/selected-deal.js
+++ scripts/selected-deal.js
@@ -4,40 +4,77 @@ const selectedDealEmailButton = document.getElementById('selected-deal-email-but
 const selectedDealSendMethod = document.querySelector('.selected-deal-send-method');
 const selectedDealLabel = document.getElementById('selected-deal-label');
 const selectedDealResponse = document.querySelector('.selected-deal-response');
-const selectedDealSwitchRedemptionMethod = document.querySelector('.selected-deal-switch-redemption-method');
+const selectedDealTextRedemption = document.querySelector('.selected-deal-text-redemption');
+const selectedDealEmailRedemption = document.querySelector('.selected-deal-email-redemption');
+const shareDealButton = document.getElementById('share-deal-button');
+const favoriteDealButton = document.getElementById('favorite-deal-button');
+const selectedDealShareContainer = document.getElementById('selected-deal-share-container');
+const selectedDealFavoriteContainer = document.getElementById('selected-deal-favorite-container');
+const closeShareButton = document.getElementById('close-share-button');
 
+// shows text display when text button is clicked
 function showTextChoices(){
   selectedDealLabel.innerText = 'Send the coupon via text.';
   selectedDealResponse.innerText = 'Use or enter new phone number.';
   selectedDealResponse.style.opacity = '1';
-  selectedDealSwitchRedemptionMethod.innerText = 'Send via email.';
-  selectedDealSwitchRedemptionMethod.style.opacity = '1';
+  selectedDealEmailRedemption.innerText = 'Send via email.';
+  selectedDealEmailRedemption.style.display = 'inline';
+  selectedDealTextRedemption.style.display = 'none';
   selectedDealSendMethod.style.height = '50px';
   selectedDealSendMethod.style.border = 'solid 1px #000';
   selectedDealSendButton.style.backgroundColor = '#FF0000';
+  selectedDealSendButton.style.margin = '15px 0';
   selectedDealSendButton.disabled = false;
+  selectedDealEmailRedemption.addEventListener('click', showEmailChoices);
 }
 
+// shows email display when email button is clicked
 function showEmailChoices(){
   selectedDealLabel.innerText = 'Send the coupon via email.';
   selectedDealResponse.innerText = 'Use or enter new email address.';
   selectedDealResponse.style.opacity = '1';
-  selectedDealSwitchRedemptionMethod.innerText = 'Send via text.';
-  selectedDealSwitchRedemptionMethod.style.opacity = '1';
+  selectedDealTextRedemption.innerText = 'Send via text.';
+  selectedDealTextRedemption.style.display = 'inline';
+  selectedDealEmailRedemption.style.display = 'none';
   selectedDealSendMethod.style.height = '50px';
   selectedDealSendMethod.style.border = 'solid 1px #000';
   selectedDealSendButton.style.backgroundColor = '#FF0000';
+  selectedDealSendButton.style.margin = '15px 0';
   selectedDealSendButton.disabled = false;
+  selectedDealTextRedemption.addEventListener('click', showTextChoices);
 }
 
+shareDealButton.addEventListener('click', () => {
+  selectedDealShareContainer.style.top = '125px';
+  selectedDealShareContainer.style.opacity = '1';
+  selectedDealFavoriteContainer.style.top = '-500px';
+  selectedDealFavoriteContainer.style.opacity = '0';
+  if(window.innerWidth <= 700){
+    selectedDealShareContainer.style.top = '75px';
+  }
+});
+
+closeShareButton.addEventListener('click', () => {
+  selectedDealShareContainer.style.top = '-500px';
+});
+
+favoriteDealButton.addEventListener('click', () => {
+  selectedDealFavoriteContainer.style.top = '125px';
+  selectedDealFavoriteContainer.style.opacity = '1';
+  selectedDealShareContainer.style.top = '-500px';
+  selectedDealShareContainer.style.opacity = '0';
+  if(window.innerWidth <= 700){
+    selectedDealFavoriteContainer.style.top = '75px';
+  }
+});
+
+
+
+// text button
 selectedDealTextButton.addEventListener('click', () => {
   showTextChoices();
-  selectedDealSwitchRedemptionMethod.addEventListener('mouseover', () => {
-    selectedDealSwitchRedemptionMethod.style.opacity = '0.75';
-  });
-  selectedDealSwitchRedemptionMethod.addEventListener('mouseout', () => {
-    selectedDealSwitchRedemptionMethod.style.opacity = '1';
-  });
+  selectedDealTextButton.style.display = 'none';
+  selectedDealEmailButton.style.display = 'none';
   selectedDealSendButton.addEventListener('mouseover', () => {
     selectedDealSendButton.style.backgroundColor = '#000';
     selectedDealSendButton.style.cursor = 'pointer';
@@ -47,14 +84,11 @@ selectedDealTextButton.addEventListener('click', () => {
   });
 });
 
+// email button
 selectedDealEmailButton.addEventListener('click', () => {
   showEmailChoices();
-  selectedDealSwitchRedemptionMethod.addEventListener('mouseover', () => {
-    selectedDealSwitchRedemptionMethod.style.opacity = '0.75';
-  });
-  selectedDealSwitchRedemptionMethod.addEventListener('mouseout', () => {
-    selectedDealSwitchRedemptionMethod.style.opacity = '1';
-  });
+  selectedDealTextButton.style.display = 'none';
+  selectedDealEmailButton.style.display = 'none';
   selectedDealSendButton.addEventListener('mouseover', () => {
     selectedDealSendButton.style.backgroundColor = '#000';
     selectedDealSendButton.style.cursor = 'pointer';
@@ -64,9 +98,11 @@ selectedDealEmailButton.addEventListener('click', () => {
   });
 });
 
-async function selectedDeal(url, img, name, discount){
-    console.log(url);
-    console.log(img);
-    console.log(name);
-    console.log(discount);
+function selectedDeal(url, img, name, discount){
+  const newDeal = {
+    dealImg: img,
+    dealName: name,
+    dealDiscount: discount
+  };
+    console.log(newDeal);
 }
