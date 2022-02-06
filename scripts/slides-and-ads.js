d1f70650904ab7559e0aaab6ba7d3ebcb24cb496
Nate M <jeditendencies35@gmail.com>
2022-02-06 00:47:52 -0500

    organized css and removed un-needed styles

--- scripts/slides-and-ads.js
+++ scripts/slides-and-ads.js
@@ -1,42 +1,8 @@
 // HOME v3.0
 // This file handles top banner scrolling/fading and ads rotating/fading on index.html.
 
-// const slides = document.getElementsByClassName("slides");
 const ad = document.getElementById('ad');
 const adLink = document.getElementById('ad-link');
-// let slides = document.querySelectorAll(".banner-slides");
-// const prev = document.getElementById('prev');
-// const next = document.getElementById('next');
-
-// initial slide index of 1
-// let slideIndex = 1;
-// showSlides(slideIndex);
-//
-// next/previous controls
-// function nextSlide() {
-//   showSlides(slideIndex += 1);
-// }
-//
-// thumbnail image controls
-// function previousSlide() {
-//   showSlides(slideIndex -= 1);
-// }
-//
-// shuffles through slides
-// function showSlides(n) {
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//     slides[i].style.position = 'absolute';
-//     slides[i].style.zIndex = '-1';
-//     slides[i].style.opacity = '0';
-//   }
-//   slides[slideIndex-1].style.display = "grid";
-//   slides[slideIndex-1].style.position = 'relative';
-//   slides[slideIndex-1].style.zIndex = '1';
-//   slides[slideIndex-1].style.opacity = '1';
-// }
 
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
@@ -71,7 +37,6 @@ bannerSlideContainer.addEventListener("transitionend", () => {
   }
 });
 
-
 // populates ad banner with logo images from coupon data in data.js, shows for 5 seconds, fades out for 3, repeats
 function showAds(){
   const randomImages =  adImages[Math.floor(Math.random() * adImages.length)];
@@ -89,5 +54,3 @@ function showAds(){
 
 // scrolls ads and loads banner on page load
 window.addEventListener('load', showAds);
-// prev.addEventListener('click', previousSlide);
-// next.addEventListener('click', nextSlide);
